b43515d606793cee9c90e3f95bc9f690663d7dff
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-03-15 02:14:37 -0300

    add font map, add color map, add time map

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
+const burger = document.querySelector('.burger');
+const navlinks = document.querySelectorAll('.navbar__links a');
+const navbar = document.querySelector('.navbar__links');
 
-
-
-const hello = 'hello'
-
-const bye = () => {
-  console.log(hello)
+const toggle = () => {
+  burger.addEventListener('click', () => {
+    navbar.classList.toggle('nav-active')
+    burger.classList.toggle('toggle')
+    navlinks.forEach(link, index => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.styles.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+      }
+    });
+  });
 }
+
+toggle();
