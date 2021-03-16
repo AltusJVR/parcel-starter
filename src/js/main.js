62f4aee52b9e1ee5443509a229d4f51c256b4e66
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-03-15 21:08:49 -0300

    Update, colors, typo, layout, mixins, mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,36 @@
 const burger = document.querySelector('.burger');
 const navlinks = document.querySelectorAll('.navbar__links a');
 const navbar = document.querySelector('.navbar__links');
+const nav = document.querySelector('.navbar');
 
+
+/* Toggle mobile navbar fade in. Toggle Navbar burger animation */
 const toggle = () => {
   burger.addEventListener('click', () => {
     navbar.classList.toggle('nav-active')
     burger.classList.toggle('toggle')
-    navlinks.forEach(link, index => {
+    navlinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = ''
       } else {
-        link.styles.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
       }
     });
   });
+};
+
+/* Toggle navbar opacity */
+const navOpacity = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    if (window.scrollY <= 150) {
+      nav.classList.add('navOpacity')
+    } else {
+      nav.classList.remove('navOpacity')
+    }
+  })
 }
 
+
+navOpacity();
 toggle();
