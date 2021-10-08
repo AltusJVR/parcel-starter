bbbb018328f007f773265906bf110a5ed2e495ab
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-10-07 22:14:41 -0300

    bump parcel to nighty 850, add type to js scripts

--- src/js/style.js
+++ src/js/style.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
-import '../scss/layout/contact.scss';
-import '../scss/layout/mobile.scss';
+import "../scss/main.scss";
+import "../scss/layout/contact.scss";
+import "../scss/layout/mobile.scss";
 // import { imageUrl } from './image?quality=75';
